87e121b9578a9b6ce6f18eb6ae681ef79d44643f
McKB <mckennakb@comcast.net>
2022-06-05 10:49:43 -0400

    genres controller running

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,9 +1,38 @@
-const getAllGenres = () => {
+const models = require('../models')
 
+const getAllGenres = async (req, res) => {
+  try {
+    const allGenres = await models.Genres.findAll({
+      attributes: { exclude: ['deletedAt'] }
+    })
+
+    return allGenres
+      ? res.status(200).send(allGenres)
+      : res.sendStatus(404)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
 }
 
-const getGenreByIdWithNovelsAuthors = () => {
+const getGenreByIdWithNovelsAuthors = async (req, res) => {
+  try {
+    const { id } = req.params
+    const genre = await models.Genres.findOne({
+      where: { id },
+      include: [{
+        model: models.Novels,
+        include: [{
+          model: models.Authors,
+        }]
+      }]
+    })
 
+    return genre
+      ? res.status(200).send(genre)
+      : res.sendStatus(404)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
 }
 
 module.exports = { getAllGenres, getGenreByIdWithNovelsAuthors }

fee85f1bc395b3b90c267d7a0bc305f2b867ef3c
McKB <mckennakb@comcast.net>
2022-06-08 20:23:16 -0400

    sets up .env and dynamic configuration

--- models/index.js
+++ models/index.js
@@ -1,12 +1,16 @@
 const Sequelize = require('sequelize')
+const config = require('../config/sequelize')
 const authors = require('./authors')
 const genres = require('./genres')
 const novels = require('./novels')
 const novelsGenres = require('./novelsGenres')
 
-const connection = new Sequelize('great_novels', 'great_novels_user', 'bookworm', {
-  host: 'localhost',
-  dialect: 'mysql'
+const environment = process.env.NODE_ENV || 'development'
+const { username, password, database, host, dialect } = config[environment] //eslint-disable-line
+
+const connection = new Sequelize(database, username, password, {
+  host: host,
+  dialect: dialect
 })
 
 const Authors = authors(connection, Sequelize)
@@ -21,4 +25,10 @@ Novels.belongsTo(Authors)
 Genres.belongsToMany(Novels, { through: NovelsGenres })
 Novels.belongsToMany(Genres, { through: NovelsGenres })
 
-module.exports = { Authors, Genres, Novels, NovelsGenres }
+module.exports = {
+  Authors,
+  Genres,
+  Novels,
+  NovelsGenres,
+  Op: Sequelize.Op
+}
